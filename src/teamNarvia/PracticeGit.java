ee18d17b078196dccb8ff1403c35c2727a0e12e1
avfil <avfilatova143@gmail.com>
2023-01-12 17:54:42 -0500

    Created class Practice Git and add comment

--- src/teamNarvia/PracticeGit.java
+++ src/teamNarvia/PracticeGit.java
@@ -3,6 +3,7 @@ package teamNarvia;
 public class PracticeGit {
     public static void main(String[] args) {
         System.out.println("Enter codes");
+        System.out.println("Great job");
 
     }
 }
