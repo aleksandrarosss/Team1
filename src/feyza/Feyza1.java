ae779a67a10167bed6515399487ef9eeb6cda41e
faygen <feyzauzunaygen@gmail.com>
2023-01-12 17:54:24 -0500

    second commit

---
+++ src/feyza/Feyza1.java
@@ -0,0 +1,12 @@
+package feyza;
+
+public class Feyza1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Wonderfull day");
+        System.out.println("hello");
+        System.out.println("great");
+        System.out.println("batch 28");
+    }
+}
