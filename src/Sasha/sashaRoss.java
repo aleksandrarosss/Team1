6be36dd7641870491defa9319d3ce7d4f1cc0fd4
avfil <avfilatova143@gmail.com>
2023-01-12 17:39:21 -0500

    added comment2

--- src/Sasha/sashaRoss.java
+++ src/Sasha/sashaRoss.java
@@ -4,5 +4,6 @@ public class sashaRoss {
     public static void main(String[] args) {
         System.out.println("hello team");
         System.out.println("It sooo hard)))");
+        System.out.println("but it get esier0000");
     }
 }
