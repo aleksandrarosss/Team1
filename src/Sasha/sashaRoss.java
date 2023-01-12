86c5c71515d9dd9d4fff7bc58d860fae5b534fa4
avfil <avfilatova143@gmail.com>
2023-01-12 17:38:45 -0500

    added comment

--- src/Sasha/sashaRoss.java
+++ src/Sasha/sashaRoss.java
@@ -3,5 +3,6 @@ package Sasha;
 public class sashaRoss {
     public static void main(String[] args) {
         System.out.println("hello team");
+        System.out.println("It sooo hard)))");
     }
 }
